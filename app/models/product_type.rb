f5fd3fed257fddce5604cd11efcf44a0a46fec50
Joseph Cabral <joe1363_2001@yahoo.com>
2016-01-18 10:22:53 -0800

    set up product type model

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,2 +1,5 @@
 class ProductType < ActiveRecord::Base
+  attr_accessible :name, :fields_attributes
+  has_many :fields, class_name: "ProductField"
+  accepts_nested_attricutes_for :fields, allow_destroy: true
 end
