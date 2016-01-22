cd75daa227b194f6c5b4d68e2a1bcc0e0c47c96e
Joseph Cabral <joe1363_2001@yahoo.com>
2016-01-22 09:54:28 -0800

    worked on product index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
 
   # GET /products/new
   def new
-    @product = Product.new
+    @product = Product.new(product_type_id: params[:product_type_id])
   end
 
   # GET /products/1/edit
