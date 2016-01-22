ec76314120320976f94a031ca0525ddba4904723
Joseph Cabral <joe1363_2001@yahoo.com>
2016-01-22 13:26:23 -0800

    added shirt product type

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:id, :name, :price, :product_type_id, :properties => [:Author, "Page Count"])
+      params.require(:product).permit(:id, :name, :price, :product_type_id, :properties => [:Author, "Page Count", :available_sizes => []])
     end
 end
