25591a8f30c3428f0f1336bcef99127c9622637d
Joseph Cabral <joe1363_2001@yahoo.com>
2016-01-22 09:57:09 -0800

    added product type id and properties columns to products table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121195059) do
+ActiveRecord::Schema.define(version: 20160122175534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,10 @@ ActiveRecord::Schema.define(version: 20160121195059) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.decimal  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "product_type_id"
+    t.text     "properties"
   end
 
   add_foreign_key "product_fields", "product_types"
